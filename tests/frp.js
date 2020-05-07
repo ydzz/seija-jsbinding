44dc2197d76b68da3ad4c3dc07b4129f4b65f1ec
yangdao <=>
2020-05-07 23:32:57 +0800

    new frp

---
+++ tests/frp.js
@@ -0,0 +1,90 @@
+import {g2d} from "seija";
+
+export function defaultEvent() {
+    var retObject = {
+        nextEvents:[],
+        behavoirs:[],
+        func:null,
+        onFire: function(val) {
+            var newVal = val;
+            if(this.func != null) {
+                newVal = this.func(val);
+            }
+            for(var i = 0;i < this.nextEvents.length;i++) {
+                var curEvent = this.nextEvents[i];
+                curEvent.onFire(newVal);
+            }
+
+            for(var i = 0;i < this.behavoirs.length;i++) {
+                var curBehavior = this.behavoirs[i];
+                curBehavior.onValue(newVal);
+            }
+
+
+        }
+    };
+    retObject.onFire.bind(retObject);
+    return retObject;
+}
+
+export function fetchEvent(world,entity,eventType,isCapture) {
+    var newEvent = defaultEvent();
+    g2d.attachNodeEvent(world,entity,eventType,isCapture,newEvent);
+    return newEvent;
+}
+
+export function chainEvent(ev,fn) {
+    var newEvent = defaultEvent();
+    newEvent.func = fn;
+    ev.nextEvents.push(newEvent);
+    return newEvent;
+}
+
+
+export function newBehavior(val) {
+    var retObject = {
+        value:val,
+        foldFunc:null,
+        callBack:null,
+        attachInfo:null,
+        attchCallback:null,
+        onValue: function(eVal) {
+            if(this.foldFunc != null) {
+                this.value = this.foldFunc(this.value,eVal);
+            } else {
+                this.value = eVal;
+            }
+            if(this.callBack != null) {
+                this.callBack(this.value);
+            }
+            if(this.attchCallback != null) {
+                this.attchCallback(this.value);
+            }
+        }
+    };
+    retObject.onValue.bind(retObject);
+    return retObject;
+}
+
+export function holdBehavior(val,event) {
+    var behavoir = newBehavior(val);
+    event.behavoirs.push(behavoir);
+    return behavoir;
+}
+
+export function foldBehavior(val,event,func) {
+    var behavoir = newBehavior(val);
+    event.behavoirs.push(behavoir);
+    behavoir.foldFunc = func;
+    return behavoir;
+}
+
+export function callbackBehavior(behavoir,f) {
+    behavoir.callBack = f;
+}
+
+export function setTransformBehavior(world,entity,attrType,behavoir) {
+    behavoir.attchCallback = function(val) {
+        g2d.setTransform(world,entity,attrType,val);
+    }.bind(behavoir);
+}
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,6 @@
 import {g2d,app} from "seija";
-import * as behavior from "./behavior.js";
-import * as event from "./event.js";
 import * as core from "./core.js";
+import * as frp from "./frp.js";
 
 var s2d = g2d.newSimple2d({  window: {width:1024,height:768} });
   
@@ -10,32 +9,19 @@ var myapp = app.newApp(s2d,{OnStart,ResPath: "../seija/examples/first/res/"});
 
 function OnStart(world) {
     var loader = g2d.fetchLoader(world);
-    var sheet = g2d.loadSync(loader,world,2,"111/material.json");
+    var sheet = g2d.loadSync(loader,world,2,"111/material.json",null);
     var root = core.addEventRoot(world);
     var elSprite = core.mkSprite(world,sheet,"button",{pos:[0,0,0],size:[100,100] },root);
-    var eClick = g2d.getEvent(world,elSprite,2,false);
-
-    //testBehavior(eClick);
-     //removeAllChildren
-   core.chainEvent(eClick,function(e) {
-     var idList = g2d.getChildrens(world,root);
-     console.error(idList);
-     g2d.removeAllChildren(world,root);
-     return e;
-   });
-}
-
-function testBehavior(eClick) {
-    behavior.testNumberBehavior(eClick);
-    behavior.testStringBehavior(eClick);
-    behavior.testObjectBehavior(eClick);
-    behavior.testFunctionBehavior(eClick);
-    behavior.testArrayBehavior(eClick);
-}
-
-function testEvent(eClick) {
-    event.testNormalEvent(eClick);
-    event.testSetNextEvent(eClick);
+    
+    var eClick = frp.fetchEvent(world,elSprite,2,false);
+  
+    
+    var behavior = frp.foldBehavior([0,0,0],eClick,function(v,ev){
+        v[0] += 1;
+        return v;
+    });
+    
+    frp.setTransformBehavior(world,elSprite,0,behavior);
 }
 
 
